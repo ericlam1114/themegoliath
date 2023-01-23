1ee38ada67132cf742e05d7251d87901cac49aa0
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-01-23 09:08:56 -0800

    width n height update

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Header from '../component/Header'
 
 export default function Home() {
   return (
-   <div className='bg-gradient-to-r from-yellow-100 to-amber-300 pb-20'>
+   <div className='bg-gradient-to-r from-yellow-100 to-amber-300 pb-20 w-full h-full'>
     <Header></Header>
 <Hero/>
    </div>
